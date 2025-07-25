b20eca06b261c2e2f98332e131de2cd571088e9b
Vikram Venkatraghavan <88.vikram@gmail.com>
2025-07-25 15:20:54 +0200

    setup updated

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+requirements = (this_directory / "requirements.txt").read_text().splitlines()
 
 setuptools.setup(
     name="snowphlake",
@@ -8,23 +12,13 @@ setuptools.setup(
     description="Personalized Disease Progression Model",
     long_description="Staging NeurOdegeneration With PHenotype informed progression timeLine of biomarKErs",
     long_description_content_type="text/markdown",
-    url="https://github.com/88vikram/snowphlake",
+    url="https://github.com/snowphlake-dpm/snowphlake",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=[
-      'numpy',
-      'pandas',
-      'scikit-learn',
-      'scipy',
-      'matplotlib',
-      'rpy2',
-      'nimfa',
-      'pathos',
-      'plotly'
-    ],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
